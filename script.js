88f4b84eda96e1f586160dfbb89058f257932dc9
Sophia Gallindo <67246528+Sophia-15@users.noreply.github.com>
2021-06-10 14:28:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,17 +15,19 @@ function cal(){
     console.log(imc)
     resultado.innerHTML = `${imc}`
     if (imc <= 18.5){
-        mensagem.innerHTML = `Você está abaixo do peso`
+        mensagem.innerHTML = 'Você está abaixo do peso'
     }else if (imc > 18.6 && imc < 24.8){
-        mensagem.innerHTML = `Você está no peso ideal`
+        mensagem.innerHTML = 'Você está no peso ideal'
     }else if (imc > 25.0 && imc < 29.9){
-        mensagem.innerHTML = `Você está levemente acima do peso`
+        mensagem.innerHTML = 'Você está levemente acima do peso'
     }else if (imc > 30.0 && imc < 34.9){
-        mensagem.innerHTML = `Você está na obesidade grau I`
+        mensagem.innerHTML = 'Você está na obesidade grau I'
     }else if (imc > 35.0 && imc < 39.9){
-        mensagem.innerHTML = `Você está na obesidade grau II`
+        mensagem.innerHTML = 'Você está na obesidade grau II'
     }else if (imc >= 40.0){
-        mensagem.innerHTML = `Você está na obesidade grau III`
+        mensagem.innerHTML = 'Você está na obesidade grau III'
+    }else if (imc === 0.0){
+        mensagem.innerHTML = 'Verifique se você botou os dados corretos!'
     }else {
         mensagem.innerHTML = 'Por favor, insira seus dados!'
     }
